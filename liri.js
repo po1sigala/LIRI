e778235707d3b81ddc47eaef921905ca40f983a0
Your Name <alexsigala87@gmail.com>
2019-05-17 23:38:56 -0700

    all searches IMDB and bands in town are fully functionable. Spotify query us workng but need to add the right response objects to our console log. the do what it says string is available but need to have it run through spotify this once spotify this is fully coded

--- liri.js
+++ liri.js
@@ -11,6 +11,9 @@ var axios= require("axios");
 //require fs to read and write files
 var fs= require("fs");
 
+//make new object from spotify consructor with api keys from our hidden file
+var spotify= new Spotify(keys.spotify);
+
 //set up inquirer to ask what command we want to run so our user doesnt have to type it exactly. aka no typos
 inquirer.prompt([
     //first object asks which API the user wants to check
@@ -20,22 +23,27 @@ inquirer.prompt([
     message:"what do you want to do?",
     choices:["concert-this","spotify-this-song","movie-this","do-what-it-says"]
     },
-    //second object asks them to input the search 
+    //second object asks them to input the search
+    //need to make it so that the next prompt doesnt occur if we select dowhatitsays
+     
     {
         type: "input",
         name: "search",
         message: "What do you want to search?"
       }
 ])
-.then(function(user){
+.then(function searchSites(user){
 //set out command variable that decides what we wnat to do. mayve switch this to be based on the answer of a prompt?
 var searchSelect=user.searchSelect;
 
 var search= user.search;
 
     // take in arguments from command line for concerts spotify and movies and dowhat it says
+decider(searchSelect,search);
 
+})
 
+function decider(searchSelect,search){
     if(searchSelect==="concert-this"){
         var queryUrl="https://rest.bandsintown.com/artists/" 
                     + search 
@@ -55,7 +63,17 @@ var search= user.search;
 
     };
     if(searchSelect==="spotify-this-song"){
-        var queryUrl= "";
+        console.log("the search was "+search);
+        spotify.search({
+            
+            type: 'track', query: "All the Small Things" }, function(err, data) {
+                if (err) {
+                  return ('Error occurred: ' + err);
+                }
+               console.log("the search was "+search);
+              console.log(data);
+        })
+        console.log("spotify if statment came back true")
     };
     if(searchSelect==="movie-this"){
         var queryUrl= "http://www.omdbapi.com/?t=" +
@@ -75,14 +93,30 @@ var search= user.search;
             }
         )
     };
+    //have a seperate function that reads a file and executes its command
     if(searchSelect==="do-what-it-says"){
-
+        //read file
+        fs.readFile("random.txt","utf8",function(err,data){
+            //return error if any
+            if(err){
+                return(err);
+            }
+            //turn string into array. 
+            data=data.split(", ");
+            console.log("the data is "+data);
+            //set the first object in the array to the search select var
+            searchSelect=data[0];
+            console.log("the var searchSelectis "+searchSelect);
+            //set the second object to the search variable
+            search=data[1];
+            console.log("the var search is "+search);
+            //pass these variables into this function again but now one of the other if statements will run true
+            decider(searchSelect,search);
+            
+        })
     };
-})
+}
 
-
-//make new object from spotify consructor with api keys from our hidden file
-var spotify= new Spotify(keys.spotify);
 
 
 
