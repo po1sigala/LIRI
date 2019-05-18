6be4eeaad8e2f5c5f07c4f441912b1dc093a9a79
Your Name <alexsigala87@gmail.com>
2019-05-18 12:54:41 -0700

    finished application updated the console log in spotify t his to show the appropriate info from the response. added a 'when' prop erty to the search object within prompt. this when property makes it so  it only runs when dowhatitsays is not selected. this is because do wha t it says builds the search query based off a txt file and it therefore  we shouldnt be prompting the user for a search

--- liri.js
+++ liri.js
@@ -25,11 +25,15 @@ inquirer.prompt([
     },
     //second object asks them to input the search
     //need to make it so that the next prompt doesnt occur if we select dowhatitsays
-     
+    
     {
         type: "input",
         name: "search",
-        message: "What do you want to search?"
+        message: "What do you want to search?",
+        //include a when that make sit so this prompt does not show when dowhatitsays is selecteed. we want thios becasue dowhat its says bases its search of a text so it would be redundant to ask again here
+        when: function(answers){
+            return answers.searchSelect != "do-what-it-says";
+        }
       }
 ])
 .then(function searchSites(user){
@@ -44,12 +48,16 @@ decider(searchSelect,search);
 })
 
 function decider(searchSelect,search){
+    //code for when our user wants to search a concert
     if(searchSelect==="concert-this"){
+        //query url built off search input
         var queryUrl="https://rest.bandsintown.com/artists/" 
                     + search 
                     + "/events?app_id=codingbootcamp";
+        //use axios to perform a get request on the queryurl.
         axios.get(queryUrl)
             .then(function(response){
+                //console log the bits of the response we want
                 console.log("Venue: "+ response.data[0].venue.name
                             +"\nLocation: "+response.data[0].venue.city+" "
                                          +response.data[0].venue.region+", "
@@ -63,19 +71,23 @@ function decider(searchSelect,search){
 
     };
     if(searchSelect==="spotify-this-song"){
-        console.log("the search was "+search);
+        // console.log("the search was "+search);
         spotify.search({
-            
-            type: 'track', query: "All the Small Things" }, function(err, data) {
+            //spoitfy module uses a search method which takes an obect and a function
+            type: 'track', query: search }, function(err, data) {
                 if (err) {
                   return ('Error occurred: ' + err);
                 }
-               console.log("the search was "+search);
-              console.log(data);
+               console.log("Artist:"+data.tracks.items[0].artists[0].name
+                            +"\nAlbum: "+ data.tracks.items[0].album.name
+                            +"\nTrack Title: "+data.tracks.items[0].name
+                            +"\nLink to Track: "+data.tracks.items[0].href);
+            //   console.log(data.tracks.items[0]);
         })
-        console.log("spotify if statment came back true")
+        // console.log("spotify if statment came back true")
     };
     if(searchSelect==="movie-this"){
+        //same style as concert search
         var queryUrl= "http://www.omdbapi.com/?t=" +
                         search +
                         "&y=&plot=short&apikey=trilogy";
@@ -102,15 +114,20 @@ function decider(searchSelect,search){
                 return(err);
             }
             //turn string into array. 
-            data=data.split(", ");
-            console.log("the data is "+data);
+            data=data.split(",");
+            // console.log("the data array is "+data);
+
+
             //set the first object in the array to the search select var
             searchSelect=data[0];
-            console.log("the var searchSelectis "+searchSelect);
-            //set the second object to the search variable
+            // console.log("the var searchSelectis "+searchSelect)+"\nIt should be 'spotify-this-song'";
+
+
+            // //set the second object to the search variable
             search=data[1];
-            console.log("the var search is "+search);
-            //pass these variables into this function again but now one of the other if statements will run true
+            // console.log("the var search is "+search)+"it should be 'I Want it That Way'";
+            
+            // //pass these variables into this function again but now one of the other if statements will run true
             decider(searchSelect,search);
             
         })
